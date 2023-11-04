aaa21b9b1daf4727f2ee263ddb3a72213591f1a0
Andrey-Say <ansayapin83@gmail.com>
2023-11-04 07:27:53 -0400

    Create task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+const userInfo = {};
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  email === '' || password === ''
+    ? alert('Fields must be completed')
+    : (userInfo.email = email),
+    (userInfo.password = password);
+
+  event.currentTarget.reset();
+}
+console.log(userInfo);
