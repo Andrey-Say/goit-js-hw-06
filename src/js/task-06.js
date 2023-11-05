a253e17106fc720c8fefb2909953fe2a0c91b88a
Andrey-Say <ansayapin83@gmail.com>
2023-11-05 01:59:12 -0400

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,7 +3,16 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onValidationInput);
 
 function onValidationInput(evt) {
-  evt.currentTarget.value.length === Number(evt.currentTarget.dataset.length)
-    ? inputEl.classList.add('valid')
-    : inputEl.classList.add('invalid');
+  const event = evt.currentTarget;
+  // evt.currentTarget.value.length === Number(evt.currentTarget.dataset.length)
+  //   ? inputEl.classList.add('valid')
+  //   : inputEl.classList.add('invalid');
+
+  if (event.value.length === Number(event.dataset.length)) {
+    event.classList.remove('invalid');
+
+    event.classList.add('valid');
+    return;
+  }
+  event.classList.add('invalid');
 }
